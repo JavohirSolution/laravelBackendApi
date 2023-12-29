708051d8f527b4b33d71682867c412ad2a83cd7b
JavohirSolution <javohirdeveloper12@gmail.com>
2023-12-29 11:28:00 +0500

    first commit

---
+++ app/Http/Controllers/OrderyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ordery;
+use App\Http\Requests\StoreOrderyRequest;
+use App\Http\Requests\UpdateOrderyRequest;
+
+class OrderyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreOrderyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ordery $ordery)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Ordery $ordery)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateOrderyRequest $request, Ordery $ordery)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ordery $ordery)
+    {
+        //
+    }
+}
